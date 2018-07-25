dd104637749c0ab93907f66b8ad80dc58a954456
Juan Bressan <juan.bressan@rescue.org>
2018-07-25 10:37:48 -0300

    contentful/transifex staging environments set up

--- src/config.js
+++ src/config.js
@@ -1,12 +1,15 @@
 module.exports = {
 	transifexToSpaceDictionary: {
 		"refugeeinfo-refresh": "ktglofo8nf88",
+		"refugeeinfo-staging": "1q5uqv62m69g",
 	},
 	contenfulLanguageDictionary: {
 		ar: "ar-001",
 		fa: "fa-AF",
+		en: "en",
 	},
 	contentfulPrimaryLanguage: {
 		"ktglofo8nf88": "en",
+		"1q5uqv62m69g": "en",
 	},
 };
