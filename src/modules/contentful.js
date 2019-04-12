809abb813b977a730c2bcdd8e011047f0138a906
Juan Bressan <juan.bressan@rescue.org>
2019-04-12 16:08:58 -0300

    SP-654: Transifex api endpoint update

--- src/modules/contentful.js
+++ src/modules/contentful.js
@@ -20,6 +20,7 @@ const md = new Remarkable("full", {
 const TRANSIFEX_API_KEY = process.env.TRANSIFEX_API_KEY;
 const CONTENTFUL_API_TOKEN = process.env.CONTENTFUL_API_TOKEN;
 const TRANSIFEX_API_URL = "https://www.transifex.com/api/2/project";
+const TRANSIFEX_NEW_API_URL = "https://api.transifex.com/organizations";
 let {
     transifexToSpaceDictionary,
     contenfulLanguageDictionary,
@@ -101,7 +102,8 @@ function importArticleAndVideo(req, space) {
 
         let promise = new Promise((resolve, reject) => {
             request
-                .get(`${TRANSIFEX_API_URL}/${project}/resource/${slug}/`, (__e, r, __b) => {
+                .get(`${TRANSIFEX_NEW_API_URL}/refugeeinfo/projects/${project}/resources/${slug}/`, (__e, r, __b) => {
+                // .get(`${TRANSIFEX_API_URL}/${project}/resource/${slug}/`, (__e, r, __b) => {
                     let method = r.statusCode === 404 ? "POST" : "PUT";
                     let uri =
                         r.statusCode === 404 ? `${TRANSIFEX_API_URL}/${project}/resources/` : `${TRANSIFEX_API_URL}/${project}/resource/${slug}/content/`;
@@ -312,7 +314,8 @@ function uploadCategoriesToTransifex(client, spaceId) {
         };
 
         request
-            .get(`${TRANSIFEX_API_URL}/${project}/resource/${slug}/`, (__e, r, __b) => {
+            .get(`${TRANSIFEX_NEW_API_URL}/refugeeinfo/projects/${project}/resources/${slug}/`, (__e, r, __b) => {
+            // .get(`${TRANSIFEX_API_URL}/${project}/resource/${slug}/`, (__e, r, __b) => {
                 let method = r.statusCode === 404 ? "POST" : "PUT";
                 let uri = r.statusCode === 404 ? `${TRANSIFEX_API_URL}/${project}/resources/` : `${TRANSIFEX_API_URL}/${project}/resource/${slug}/content/`;
                 console.log(r.statusCode, method, uri);
